e19de4349124602248952293948441083d1d7791
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-09 14:45:57 -0600

    Updates for heroku deploy

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule QuantifiedSelfPhoenix.UserSocket do
   # channel "room:*", QuantifiedSelfPhoenix.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
