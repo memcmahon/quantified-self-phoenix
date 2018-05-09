6a0f6f807051f8853d3393d6e84ad8ec22d1e476
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-09 14:31:39 -0600

    Adds seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,43 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.SomeModel{})
+QuantifiedSelfPhoenix.Repo.delete_all(QuantifiedSelfPhoenix.MealFood)
+QuantifiedSelfPhoenix.Repo.delete_all(QuantifiedSelfPhoenix.Food)
+QuantifiedSelfPhoenix.Repo.delete_all(QuantifiedSelfPhoenix.Meal)
+QuantifiedSelfPhoenix.Repo.query("ALTER SEQUENCE mealfoods_id_seq RESTART")
+QuantifiedSelfPhoenix.Repo.query("ALTER SEQUENCE foods_id_seq RESTART")
+QuantifiedSelfPhoenix.Repo.query("ALTER SEQUENCE meals_id_seq RESTART")
+
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Food{
+  name: "Banana",
+  calories: 35
+  })
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Food{
+  name: "Orange",
+  calories: 40
+  })
+
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Meal{
+  name: "Breakfast"
+  })
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Meal{
+  name: "Lunch"
+  })
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Meal{
+  name: "Snacks"
+  })
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.Meal{
+  name: "Dinner"
+  })
+
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.MealFood{
+  meal_id: 1,
+  food_id: 1
+  })
+QuantifiedSelfPhoenix.Repo.insert!(%QuantifiedSelfPhoenix.MealFood{
+  meal_id: 1,
+  food_id: 2
+  })
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
