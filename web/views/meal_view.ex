c63e6ea75c49e6d28b8f48028dc6759a4efbff9a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-12 13:04:19 -0600

    Updates meal view to use the proper json format

--- web/views/meal_view.ex
+++ web/views/meal_view.ex
@@ -6,7 +6,7 @@ defmodule QuantifiedSelfPhoenix.MealView do
   end
 
   def render("show.json", %{meal: meal}) do
-    %{data: render_one(meal, QuantifiedSelfPhoenix.MealView, "meal.json")}
+    render_one(meal, QuantifiedSelfPhoenix.MealView, "meal.json")
   end
 
   def render("meal.json", %{meal: meal}) do
